8ef76dd5b62213937fb4c8ae3f53115e28d13d67
Harpreet Lalwani <lalwanh@amazon.com>
2021-07-02 18:01:56 +0530

    Added Memory and Loaavg

--- agent.py
+++ agent.py
@@ -10,14 +10,39 @@ def cpu_usage():
     cpu_per=(1-(idle_time/total_time))*100
     return cpu_per
 
+def load_average():
+    return os.getloadavg()
+
+def disk_usage():
+    
+
+def memory_usage():
+    memory=open("/proc/meminfo")
+    for i in memory.readlines():
+        if i.split(":")[0] == "MemTotal":
+            MT=i.split(":")[1].strip()
+        if i.split(":")[0] == "MemFree":
+            MF=i.split(":")[1].strip()
+        if i.split(":")[0] == "SwapTotal":
+            ST=i.split(":")[1].strip()
+        if i.split(":")[0] == "SwapFree":
+            SF=i.split(":")[1].strip()
+    MemoryUsed=int(MT[:-3])-int(MF[:-3])
+    SwapUsed=int(ST[:-3])-int(SF[:-3])
+    return {"MemoryUsagein kB": MemoryUsed, "SwapUsed in kB": SwapUsed}
+
 @click.command()
 @click.option('-c', '--cpu', is_flag=True, help='Prints CPU Percentage usage')
 @click.option('-m', '--memory', is_flag=True, help='Prints Memory Percentage usage')
-def main(cpu,memory):
+@click.option('-la', '--loadavg', is_flag=True, help='Prints Load average of system')
+def main(cpu,memory,loadavg):
     if cpu:
         print(cpu_usage())
     if memory:
-        print("memory_usage()")
+        print(memory_usage())
+    if loadavg:
+        print(load_average())
+
 
 
 
